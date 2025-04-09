a24e84341e838bd17984e2730e8ef8668f152a3b
Leinadsevla <danieldacunhaalves@gmail.com>
2025-04-09 19:22:55 -0300

    segundo comit

---
+++ Variaveis.js.D/Dan2025/node_modules/exercicios4.js
@@ -0,0 +1,7 @@
+//"João, "João, o total da sua conta com taxa de serviço é R$63,58." R$63,58."
+let cliente = "Joaquim";
+let total = 60.80;
+let taxaServico = 10;
+let totalFinal = total + (total * taxaServico / 100);
+
+console.log(`${cliente},o total da sua conta com taxa de serviço é R$ ${totalFinal} .`);
